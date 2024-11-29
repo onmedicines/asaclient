6e0afa3f4f3fb18e47a0a09af6ba89df13154a09
onmedicines <semwalanurag26@gmail.com>
2024-11-30 01:00:07 +0530

    Style student login page

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <div className="h-screen min-h-screen w-screen max-w-screen text-zinc-800 flex flex-col">
       <header className="text-center text-xl sm:text-2xl font-bold py-4 bg-sky-600 text-zinc-200 ">Assignment Submission App</header>
-      <main className="flex flex-col grow">
+      <main className="flex flex-col grow bg-sky-200 py-8 px-4 justify-center">
         <Outlet />
       </main>
     </div>
--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function StudentLogin() {
   return (
-    <div className="flex flex-col gap-2 items-center mt-8">
-      <h1 className="text-3xl font-bold text-center">Student Login</h1>
+    <div className="flex flex-col gap-2 items-center py-8 rounded-md bg-white">
+      <h1 className="text-3xl font-bold text-center text-sky-600">Student Login</h1>
       <p className="text-sm">Please enter your details</p>
-      <form className="w-full flex flex-col gap-8 px-8 py-8">
-        <label htmlFor="rollNumber" className="flex flex-col w-full border-b-2 border-b-zinc-700">
-          <span className="text-sm font-bold text-zinc-500">Roll Number</span>
-          <input type="number" name="rollNumber" id="rollNumber" className="py-2 focus:outline-none" />
+      <form className="w-full flex flex-col gap-8 px-8 py-8 mt-4">
+        <label htmlFor="rollNumber" className="relative flex flex-col w-full border-b-2 border-b-zinc-700">
+          <input type="number" name="rollNumber" id="rollNumber" className="bg-inherit peer py-2 focus:outline-none placeholder-transparent" placeholder="Roll Number" />
+          <span className="text-xs font-semibold text-zinc-500 absolute -top-4 left-0 peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-focus:-top-4 peer-focus:text-xs transition-all duration-100">Roll Number</span>
         </label>
-        <label htmlFor="password" className="flex flex-col w-full border-b-2 border-b-zinc-700">
-          <span className="text-sm font-bold text-zinc-500">Password</span>
-          <input type="text" name="password" id="password" className="py-2 focus:outline-none" />
+        <label htmlFor="password" className="relative flex flex-col w-full border-b-2 border-b-zinc-700">
+          <input type="text" name="password" id="password" className="bg-inherit peer py-2 focus:outline-none placeholder-transparent" placeholder="Password" />
+          <span className="text-xs font-semibold text-zinc-500 absolute -top-4 left-0 peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-focus:-top-4 peer-focus:text-xs transition-all duration-100">Password</span>
         </label>
-        <button type="submit" className="w-full bg-sky-600 py-2 text-zinc-200 font-bold">
+        <button type="submit" className="w-full bg-sky-600 py-2 text-zinc-200 font-bold focus:outline-transparent focus:ring-4 focus:ring-sky-300 rounded-sm">
           Login
         </button>
       </form>
+      <div className="text-center text-sm">
+        <p>Not registered yet?</p>
+        <Link to="/student/register" className="underline ">
+          Register now
+        </Link>
+      </div>
     </div>
   );
 }
