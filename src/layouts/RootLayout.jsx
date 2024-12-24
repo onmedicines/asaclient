979afeb4b1a36f81fffb289b5f24aaa0d0740f8e
onmedicines <semwalanurag26@gmail.com>
2024-12-24 09:10:39 +0530

    Fix issue with layout due to error and loading components

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import { LoadingContext } from "../context/LoadingContext";
 
@@ -7,16 +8,14 @@ export default function RootLayout() {
   const [error, setError] = useState("");
 
   return (
-    <div className="h-screen min-h-screen w-full text-zinc-800 flex flex-col">
-      <div className={`min-h-full bg-white w-full max-w-screen-md rounded-md p-8 sm:p-16 flex flex-col gap-4 ${isLoading && "pointer-events-none cursor-wait"}`}>
-        {isLoading && (
-          <div className="text-center fixed right-4 bottom-4 rounded-sm w-fit bg-sky-500 p-4 text-white">
-            <p>Loading...</p>
-            <p>(It may take some time if the file is large in size)</p>
-          </div>
-        )}
-        {error && <p className="text-center fixed right-4 bottom-4 rounded-sm w-fit bg-red-500 p-4 text-white">{error}</p>}
-      </div>
+    <div className={`h-screen min-h-screen w-full text-zinc-800 flex flex-col ${isLoading && "pointer-events-none cursor-wait"}`}>
+      {isLoading && (
+        <div className="text-center fixed right-4 bottom-4 rounded-sm w-fit bg-sky-500 p-4 text-white">
+          <p>Loading...</p>
+          <p>(It may take some time if the file is large in size)</p>
+        </div>
+      )}
+      {error && <p className="text-center fixed right-4 bottom-4 rounded-sm w-fit bg-red-500 p-4 text-white">{error}</p>}
 
       <header className="text-center text-xl sm:text-2xl font-bold py-4 bg-sky-600 text-zinc-200 ">Assignment Submission App</header>
       <main className="relative flex flex-col grow bg-sky-200 py-8 px-4 justify-center items-center">
