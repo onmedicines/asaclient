bb9abb49caec21ff1cdbee01aff1e6ff848bb35f
onmedicines <semwalanurag26@gmail.com>
2024-12-27 19:06:25 +0530

    Refactor - Make SearchSVG a separate component

--- src/pages/faculty/SearchByRoll.jsx
+++ src/pages/faculty/SearchByRoll.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { LoadingContext } from "../../context/LoadingContext";
+import { SearchSVG } from "../../components/SearchSVG";
 
 export default function SearchByRoll() {
   const { setIsLoading, setError } = useContext(LoadingContext);
@@ -117,12 +118,3 @@ export default function SearchByRoll() {
     </div>
   );
 }
-
-const SearchSVG = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search">
-      <circle cx="11" cy="11" r="8"></circle>
-      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-    </svg>
-  );
-};
